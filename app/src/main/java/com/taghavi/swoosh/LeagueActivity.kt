a30b784eb48191dae917bd939a5d1ca5327b35d0
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-26 22:22:00 +0330

    League Activity Toggle Selector

--- app/src/main/java/com/taghavi/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/taghavi/swoosh/LeagueActivity.kt
@@ -4,12 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
+    }
+
+    fun onMensClicked(view: View) {
+        womensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+    }
+
+    fun onWomensClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        coedLeagueButton.isChecked = false
+    }
+
+    fun onCoedClicked(view: View) {
+        mensLeagueButton.isChecked = false
+        womensLeagueButton.isChecked = false
     }
 
     fun leagueNextClicked(view:View){
