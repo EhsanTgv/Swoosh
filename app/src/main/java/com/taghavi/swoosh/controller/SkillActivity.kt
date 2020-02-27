fc164c0d23da7a602d1a54517c7155fc8c21d868
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-27 22:33:58 +0330

    Skill Activity Toggle Selector

--- app/src/main/java/com/taghavi/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/taghavi/swoosh/controller/SkillActivity.kt
@@ -2,15 +2,28 @@ package com.taghavi.swoosh.controller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.taghavi.swoosh.R
 import com.taghavi.swoosh.utilities.EXTRA_LEAGUE
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : AppCompatActivity() {
-   private var league = ""
+    var league = ""
+    var skill = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         league = intent.getStringExtra(EXTRA_LEAGUE)!!
+    }
+
+    fun onBallerClick(view: View) {
+        beginnerSkillButton.isChecked = false
+        skill = "baller"
+    }
+
+    fun onBeginnerClick(view: View) {
+        ballerSkillButton.isChecked = false
+        skill = "beginner"
     }
 }
