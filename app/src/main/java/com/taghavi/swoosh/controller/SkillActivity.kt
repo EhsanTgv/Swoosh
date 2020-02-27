824817568bb54725b005fd214ddf73050d6d52e8
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-27 22:56:35 +0330

    Set Intent in Skill Activity to Finish Activity

--- app/src/main/java/com/taghavi/swoosh/controller/SkillActivity.kt
+++ app/src/main/java/com/taghavi/swoosh/controller/SkillActivity.kt
@@ -1,10 +1,13 @@
 package com.taghavi.swoosh.controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.taghavi.swoosh.R
 import com.taghavi.swoosh.utilities.EXTRA_LEAGUE
+import com.taghavi.swoosh.utilities.EXTRA_SKILL
 import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : AppCompatActivity() {
@@ -25,5 +28,14 @@ class SkillActivity : AppCompatActivity() {
     fun onBeginnerClick(view: View) {
         ballerSkillButton.isChecked = false
         skill = "beginner"
+    }
+
+    fun onSkillFinishClicked(view: View) {
+        if (skill != "") {
+            val finishActivity = Intent(this, FinishActivity::class.java)
+            startActivity(finishActivity)
+        } else {
+            Toast.makeText(this, "Please select a skill level", Toast.LENGTH_SHORT).show()
+        }
     }
 }
