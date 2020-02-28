304e233c8cfcba7d9601490ce08c9b38dfc7976f
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-28 14:01:57 +0330

    Make Player Model Parcelable

--- app/src/main/java/com/taghavi/swoosh/models/PlayerModel.kt
+++ app/src/main/java/com/taghavi/swoosh/models/PlayerModel.kt
@@ -1,3 +1,32 @@
 package com.taghavi.swoosh.models
 
-class PlayerModel constructor(var league: String, var skill: String)
+import android.os.Parcel
+import android.os.Parcelable
+
+class PlayerModel constructor(var league: String, var skill: String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(league)
+        parcel.writeString(skill)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<PlayerModel> {
+        override fun createFromParcel(parcel: Parcel): PlayerModel {
+            return PlayerModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<PlayerModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+}
