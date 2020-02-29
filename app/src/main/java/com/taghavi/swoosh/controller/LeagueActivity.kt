7daac90756169a2c087ab5f9462cb5bdc8b4c6af
Ehsan Taghavi <EhsanTgv@gmail.com>
2020-02-29 22:09:20 +0330

    Set Instance State in League Activity

--- app/src/main/java/com/taghavi/swoosh/controller/LeagueActivity.kt
+++ app/src/main/java/com/taghavi/swoosh/controller/LeagueActivity.kt
@@ -11,35 +11,50 @@ import com.taghavi.swoosh.utilities.EXTRA_PLAYER
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : AppCompatActivity() {
-    private var player = PlayerModel("","")
+    private var player = PlayerModel("", "")
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(EXTRA_PLAYER, player)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
     }
 
-    fun onMensClicked(view:View) {
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        try {
+            super.onRestoreInstanceState(savedInstanceState)
+            player = savedInstanceState.getParcelable(EXTRA_PLAYER)!!
+        } catch (e: Exception) {
+
+        }
+    }
+
+    fun onMensClicked(view: View) {
         womensLeagueButton.isChecked = false
         coedLeagueButton.isChecked = false
 
         player.league = "mens"
     }
 
-    fun onWomensClicked(view:View) {
+    fun onWomensClicked(view: View) {
         mensLeagueButton.isChecked = false
         coedLeagueButton.isChecked = false
 
         player.league = "womens"
     }
 
-    fun onCoedClicked(view:View) {
+    fun onCoedClicked(view: View) {
         mensLeagueButton.isChecked = false
         womensLeagueButton.isChecked = false
 
         player.league = "co-ed"
     }
 
-    fun leagueNextClicked(view:View) {
+    fun leagueNextClicked(view: View) {
         if (player.league != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
             skillActivity.putExtra(EXTRA_PLAYER, player)
